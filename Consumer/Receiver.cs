59eed05c0e36b20dbcdb8cc4662d71695ab7fb3f
sebainones <sebainones@gmail.com>
2022-10-04 12:50:43 +0200

    Changed to not autoAck and to work as work queues

--- Consumer/Receiver.cs
+++ Consumer/Receiver.cs
@@ -1,11 +1,14 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
+using System.Threading;
 public class Receiver
 {
     public static void Main()
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
+
+
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
@@ -14,18 +17,35 @@ public class Receiver
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
+            
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine($"Waiting for recieving messages in host: {factory.HostName}");
 
 
             var consumer = new EventingBasicConsumer(channel);
+
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine(" [x] Received {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+
+                 // Note: it is possible to access the channel via
+                //       ((EventingBasicConsumer)sender).Model here
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                
             };
+
             channel.BasicConsume(queue: "helloQueue",
-                                 autoAck: true,
+                                 autoAck: false,
                                  consumer: consumer);
+
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
